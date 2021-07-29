5f807c3868ff12352296a157b879b7a6b777f2af
BillZong <billzong@163.com>
2021-07-29 16:40:00 +0800

    feature/contracts: /coins/{id}/contracts/{contract_address}

---
+++ _example/v3/coinsContract.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	gecko "github.com/billzong/go-gecko/v3"
+	"github.com/billzong/go-gecko/v3/types"
+)
+
+func main() {
+	cg := gecko.NewClient(nil)
+	coin, err := cg.CoinContract(types.CoinNetworkIDEthereum, "0xdac17f958d2ee523a2206206994597c13d831ec7", false, false, false, false, false, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(coin)
+}
---
+++ v3/contracts.go
@@ -0,0 +1,42 @@
+package coingecko
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/billzong/go-gecko/format"
+	"github.com/billzong/go-gecko/v3/types"
+)
+
+// Contract Coin info from contract address
+func (c *Client) CoinContract(networkID, contract string,
+	localization, tickers, marketData, communityData, developerData, sparkline bool,
+) (*types.CoinsID, error) {
+
+	if len(networkID) == 0 || len(contract) == 0 {
+		return nil, fmt.Errorf("parameters are all required")
+	}
+
+	params := url.Values{}
+	params.Add("localization", format.Bool2String(localization))
+	params.Add("tickers", format.Bool2String(tickers))
+	params.Add("market_data", format.Bool2String(marketData))
+	params.Add("community_data", format.Bool2String(communityData))
+	params.Add("developer_data", format.Bool2String(developerData))
+	params.Add("sparkline", format.Bool2String(sparkline))
+	url := fmt.Sprintf("%s/coins/%s/contract/%s?%s", baseURL, networkID, contract, params.Encode())
+	resp, err := c.MakeReq(url)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("got resp")
+
+	var data *types.CoinsID
+	err = json.Unmarshal(resp, &data)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("unmarshal resp done")
+	return data, nil
+}
