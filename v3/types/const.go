0d5ab0dca935f8490118f5eb8a71455ac26d59f1
BillZong <billzong@163.com>
2021-07-29 16:12:27 +0800

    feature/types: some constant network coin id

---
+++ v3/types/const.go
@@ -0,0 +1,10 @@
+package types
+
+const (
+	CoinNetworkIDBitcoin         = "bitcoin"
+	CoinNetworkIDEthereum        = "ethereum"
+	CoinNetworkIDEthereumClassic = "ethereum-classic"
+	CoinNetworkIDBinance         = "binancecoin"
+	CoinNetworkIDHuobi           = "huobi-token"
+	CoinNetworkIDMatic           = "matic-network"
+)
