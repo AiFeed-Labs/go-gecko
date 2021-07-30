0f00243061cad754512168087d6d7202c90b4bfa
BillZong <billzong@163.com>
2021-07-30 15:18:06 +0800

    feature/types: some constant network coin decimals

--- v3/types/const.go
+++ v3/types/const.go
@@ -8,3 +8,21 @@ const (
 	CoinNetworkIDHuobi           = "huobi-token"
 	CoinNetworkIDMatic           = "matic-network"
 )
+
+const (
+	CoinDecimalsBitcoin         = 18
+	CoinDecimalsEthereum        = 18
+	CoinDecimalsEthereumClassic = 18
+	CoinDecimalsBinance         = 18
+	CoinDecimalsHuobi           = 18
+	CoinDecimalsMatic           = 18
+)
+
+var CoinDecmialsMap = map[string]int{
+	CoinNetworkIDBitcoin:         CoinDecimalsBitcoin,
+	CoinNetworkIDEthereum:        CoinDecimalsEthereum,
+	CoinNetworkIDEthereumClassic: CoinDecimalsEthereumClassic,
+	CoinNetworkIDBinance:         CoinDecimalsBinance,
+	CoinNetworkIDHuobi:           CoinDecimalsHuobi,
+	CoinNetworkIDMatic:           CoinDecimalsMatic,
+}
